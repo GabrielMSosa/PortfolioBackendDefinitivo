ffaaa0f200899677ebd626a667a71f4633a991dd
GabrielMSosa <sosagabriel79@gmail.com>
2022-08-06 12:15:23 -0300

    agrego cors y funcionamiento del swagger

--- src/main/java/com/portfoliojwtandjpa/portfoliojwtandjpa/varentorno/VarGlobal.java
+++ src/main/java/com/portfoliojwtandjpa/portfoliojwtandjpa/varentorno/VarGlobal.java
@@ -1,7 +1,7 @@
 package com.portfoliojwtandjpa.portfoliojwtandjpa.varentorno;
 
 public class VarGlobal {
-    public static final String urlcross="https://portfolio2-a6e86.web.app/";
+    public static final String urlcross="*";
 
 
 }
